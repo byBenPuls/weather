e7ad574a3c973f99507adbd83f9a7155f18afaf1
Ben Puls <mail@benpuls.ru>
2024-09-29 22:55:20 +0500

    edit: entities

--- src/entities.py
+++ src/entities.py
@@ -1,4 +1,24 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, is_dataclass
+from datetime import datetime
+
+
+def nested_dataclass(*args, **kwargs):
+    def wrapper(cls):
+        cls = dataclass(cls, **kwargs)
+        original_init = cls.__init__
+
+        def __init__(self, *args, **kwargs):
+            for name, value in kwargs.items():
+                field_type = cls.__annotations__.get(name, None)
+                if is_dataclass(field_type) and isinstance(value, dict):
+                    new_obj = field_type(**value)
+                    kwargs[name] = new_obj
+            original_init(self, *args, **kwargs)
+
+        cls.__init__ = __init__
+        return cls
+
+    return wrapper(args[0]) if args else wrapper
 
 
 @dataclass
@@ -16,4 +36,20 @@ class IpEntity:
 
 @dataclass(frozen=True)
 class Weather:
-    pass
+    time: datetime
+    temperature: float  # in degrees of Celsius
+    relative_humidity: float  # in percents
+    weather_code: int
+    precipitation: float  # in percents
+    wind_speed: float  # in m/s
+    wind_direction: str  # in directions on russian
+
+    surface_pressure: float | None = None  # in hPa
+    feels_like: float | None = None  # in degrees of Celsius
+    is_day: bool | None = None
+
+
+@nested_dataclass
+class DailyAndWeeklyWeather:
+    daily: Weather
+    weekly: list[Weather]
